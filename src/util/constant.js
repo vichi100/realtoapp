1684cdca570bd701078edf36cbb95f1a3c72be56
vichitra rajan <vichi100@gmail.com>
2021-07-24 03:46:56 +0530

    Update constant.js

--- src/util/constant.js
+++ src/util/constant.js
@@ -1,4 +1,4 @@
 export const SERVER_URL = "http://192.168.0.100:3000";
-export const GOOGLE_PLACES_API_KEY = "AIzaSyCjoEa7haFJpjqN3akj0LkYM0zYugXFw4s" // prod
+
 
 // export const SERVER_URL = "http://192.168.0.100:3000/";// Prod
